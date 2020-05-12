33c645842ea120e4707346b20e9861f50578b3cd
Cumali Karakoc <cumali@b302.nl>
2020-05-12 09:27:05 +0200

    Initial commit

---
+++ inschrijvingen_plots.R
@@ -0,0 +1,35 @@
+library(ggplot2)
+
+sql_conn <- odbcConnect("SAS_ML_FILTERED")
+inschrijvingen_df <-
+  sqlQuery(sql_conn, "select * from Inschrijvingen")
+
+
+attach(inschrijvingen_df)
+
+feature <- LeeftijdStudent_op_Vandatum
+footer <- "LeeftijdStudent_op_Vandatum"  
+
+feature <- LeeftijdStudent_op_Totdatum
+footer <- "LeeftijdStudent_op_Totdatum"
+
+ggplot(inschrijvingen_cont, aes(x = feature)) +
+  geom_histogram(color = "black",
+                 fill = "dark turquoise",
+                 binwidth = 1) +
+  geom_vline(aes(
+    xintercept = mean(feature, na.rm = TRUE),
+    color = "mean"), linetype = "dashed", size = 1) +
+  scale_color_manual(name = "statistics",
+                     values = c(mean = "red")) +
+  scale_x_continuous(name = footer)
+
+
+#Categorical
+#Opleidingsvorm
+#InschrijfLocatie
+#VanDatum
+#TotDatum
+#InschrijfStatus
+#StudieUtslagsort
+#StudieUitslag
