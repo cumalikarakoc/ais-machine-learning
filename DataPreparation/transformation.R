75ceadb6e18cf730aca3edce238167bdc48a9cd6
refaiothman <43170083+refaiothman@users.noreply.github.com>
2020-05-26 11:52:10 +0200

    Add onvoldoendes en Toetscijfer

--- DataPreparation/transformation.R
+++ DataPreparation/transformation.R
@@ -7,61 +7,97 @@ sql_conn <- odbcConnect("SAS_ML_FILTERED")
 #===============================
 #======= Inschrijvingen ========
 #===============================
-inschrijvingen_df <- sqlQuery(sql_conn, "Select Student, Opleidingsvorm, InschrijfLocatie, InschrijfStatus,
-                              LeeftijdStudent_op_Vandatum, VanDatum from Inschrijvingen")
+inschrijvingen_df <-
+  sqlQuery(
+    sql_conn,
+    "Select Student, Opleidingsvorm, InschrijfLocatie, InschrijfStatus,
+                              LeeftijdStudent_op_Vandatum, VanDatum from Inschrijvingen"
+  )
 
 #===============================
 #= OnderwijsEenheidResultaten ==
 #===============================
-onderwijseenheidsResultaten_raw <- sqlQuery(sql_conn, "Select distinct Student, OnderwijsEenheid, Resultaat,
-                                           NrToetscijfers from OnderwijsEenheidResultaten") 
+onderwijseenheidsResultaten_raw <-
+  sqlQuery(
+    sql_conn,
+    "Select distinct Student, OnderwijsEenheid, Resultaat,
+                                           NrToetscijfers from OnderwijsEenheidResultaten"
+  )
 
 resultaat <- onderwijseenheidsResultaten_raw %>%
   group_by(Student) %>%
   select(Student, OnderwijsEenheid, Resultaat) %>%
-  spread(OnderwijsEenheid, Resultaat)%>%
-  rename(DB_Resultaat = DB, SAQ_Resultaat = SAQ, SPD_Resultaat = SPD,
-         WT_Resultaat = WT)
+  spread(OnderwijsEenheid, Resultaat) %>%
+  rename(
+    DB_Resultaat = DB,
+    SAQ_Resultaat = SAQ,
+    SPD_Resultaat = SPD,
+    WT_Resultaat = WT
+  )
 
 nrToetscijfers <- onderwijseenheidsResultaten_raw %>%
   group_by(Student) %>%
   select(Student, OnderwijsEenheid, NrToetscijfers) %>%
   spread(OnderwijsEenheid, NrToetscijfers) %>%
-  rename(DB_NrToetscijfers = DB, SAQ_NrToetscijfers = SAQ, SPD_NrToetscijfers = SPD,
-         WT_NrToetscijfers = WT)
+  rename(
+    DB_NrToetscijfers = DB,
+    SAQ_NrToetscijfers = SAQ,
+    SPD_NrToetscijfers = SPD,
+    WT_NrToetscijfers = WT
+  )
 
-onderwijseenheidsResultaten_df <- merge(resultaat, nrToetscijfers, by="Student")
+onderwijseenheidsResultaten_df <-
+  merge(resultaat, nrToetscijfers, by = "Student")
 
 
 #===============================
 #====== ToetsResultaten ========
 #===============================
-toetsResultaten_raw <- sqlQuery(sql_conn, "Select Student, OnderwijsEenheid, Resultaat, Onvoldoende
-                                           from ToetsResultaten")
+toetsResultaten_raw <-
+  sqlQuery(
+    sql_conn,
+    "Select Student, OnderwijsEenheid, Toetscode, max(Resultaat) as Resultaat, sum(Onvoldoende) as Onvoldoendes
+                                            from SAS_ML_FILTERED.dbo.ToetsResultaten
+                                            group by Student, OnderwijsEenheid, Toetscode
+                                            order by Student"
+  )
 
-# resultaat_toets <-
+gemiddeld <-
   toetsResultaten_raw %>%
-  group_by(Student) %>%
-  select(Student, OnderwijsEenheid, Resultaat) 
-  # %>%
-  spread(OnderwijsEenheid, Resultaat)%>%  rename(DB_Resultaat = DB, SAQ_Resultaat = SAQ, SPD_Resultaat = SPD,
-         WT_Resultaat = WT)
+  group_by(Student, OnderwijsEenheid) %>%
+  summarise(Gemiddeld = mean(Resultaat)) %>%
+  select(Student, OnderwijsEenheid, Gemiddeld) %>%
+  spread(OnderwijsEenheid, Gemiddeld, fill = 0) %>%
+  rename(
+    DB_Gemiddeld = DB,
+    SAQ_Gemiddeld = SAQ,
+    SPD_Gemiddeld = SPD,
+    WT_Gemiddeld = WT
+  )
 
-onvoldoendes <- onderwijseenheidsResultaten_raw %>%
-  group_by(Student) %>%
-  select(Student, OnderwijsEenheid, NrToetscijfers) %>%
-  spread(OnderwijsEenheid, NrToetscijfers) %>%
-  rename(DB_NrToetscijfers = DB, SAQ_NrToetscijfers = SAQ, SPD_NrToetscijfers = SPD,
-         WT_NrToetscijfers = WT)
+aantal_onvoldoendes <-
+  toetsResultaten_raw %>%
+  group_by(Student, OnderwijsEenheid) %>%
+  summarise(Onvoldoendes = sum(Onvoldoendes)) %>%
+  select(Student, OnderwijsEenheid, Onvoldoendes) %>%
+  spread(OnderwijsEenheid, Onvoldoendes, fill = 0) %>%
+  rename(
+    DB_Onvoldoendes = DB,
+    SAQ_Onvoldoendes = SAQ,
+    SPD_Onvoldoendes = SPD,
+    WT_Onvoldoendes = WT
+  )
 
-onderwijseenheidsResultaten_df <- merge(resultaat, nrToetscijfers, by="Student")
+
+toetsResultaten_df <-
+  merge(gemiddeld, aantal_onvoldoendes, by = "Student")
 #===============================
 #====== Aanwezigheid ===========
 #===============================
-aanwezigheid_df <- sqlQuery(sql_conn, "Select Student, AanwezigheidsType from Aanwezigheid") %>%
+aanwezigheid_df <-
+  sqlQuery(sql_conn, "Select Student, AanwezigheidsType from Aanwezigheid") %>%
   group_by(Student, AanwezigheidsType) %>%
   tally() %>%
   mutate(Perc = round(100 * n / sum(n), 2)) %>%
   select(Student, AanwezigheidsType, Perc) %>%
   spread(AanwezigheidsType, Perc, fill = 0)
-
