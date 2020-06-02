279c219116d311fefa4a4dd4193fa8f70f7a3d15
cumalikarakoc <cumalikarakoc783@hotmail.com>
2020-06-02 11:18:34 +0200

    Fix missing commas

--- Modeling/globals.R
+++ Modeling/globals.R
@@ -1,5 +1,5 @@
 # split data into training and test datasets in 60:40 ratio
 set.seed(1)
 indexes <- sample(1:nrow(risico_studenten_df), size = 0.6 * nrow(risico_studenten_df))
-train_data <- risico_studenten_df[indexes]
-test_data <- risico_studenten_df[-indexes]
+train_data <- risico_studenten_df[indexes,]
+test_data <- risico_studenten_df[-indexes,]
