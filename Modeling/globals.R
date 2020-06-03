fe625fa01044b9ea9fdae5d40d97f4b7cd97092c
cumalikarakoc <cumalikarakoc783@hotmail.com>
2020-06-03 11:35:44 +0200

    Include courses from second periode. Replace NA's with 0

--- Modeling/globals.R
+++ Modeling/globals.R
@@ -2,7 +2,8 @@
 set.seed(1)
 # remove the columns: vooropleiding, plaats and the courses from 2nd period
 risico_studenten_df_dt <- risico_studenten_df %>%
-  select(-c(5, 7, 13, 16, 17, 20, 21, 24, 25, 28))
+  select(-c(5, 7)) %>%
+  replace(is.na(.), 0)
 
 risico_studenten_df_dt$RisicoStudent <-
   as.factor(risico_studenten_df_dt$RisicoStudent)
@@ -10,3 +11,4 @@ risico_studenten_df_dt$RisicoStudent <-
 indexes <- sample(1:nrow(risico_studenten_df), size = 0.6 * nrow(risico_studenten_df_dt))
 train_data <- risico_studenten_df_dt[indexes,]
 test_data <- risico_studenten_df_dt[-indexes,]
+
--- Modeling/random-forest.R
+++ Modeling/random-forest.R
@@ -4,9 +4,6 @@ library(randomForest)
 library(rpart.plot)
 library(caret)
 
-
-attach(train_data)
-
 tree_fit <- tree(RisicoStudent ~ ., data = train_data)
 
 summary(tree_fit)
@@ -16,16 +13,13 @@ text(tree_fit , pretty = 0)
 
 pred_tree <- predict(tree_fit, test_data, type = "class")
 confusionMatrix(table(pred_tree, test_data$RisicoStudent), positive = "1")
-# Accuracy : 0.765           
-# Kappa : 0.4625
-# Sensitivity : 0.6154          
-# Specificity : 0.8397          
+# Accuracy : 0.7906                     
 
 cv_tree <- cv.tree(tree_fit, FUN = prune.misclass)
 cv_tree
 plot(cv_tree$size, cv_tree$dev, type = "b")
 
-prune_tree <- prune.misclass(tree_fit, best = 13)
+prune_tree <- prune.misclass(tree_fit, best = 8)
 plot(prune_tree)
 text(prune_tree)
 
@@ -35,22 +29,15 @@ pred_tree_pruned <- predict(prune_tree, test_data, type = "class")
 confusionMatrix(table(pred_tree_pruned, test_data$RisicoStudent), positive =
                   "1")
 # Accuracy : 0.8205
-# Kappa : 0.5714
-# Sensitivity : 0.6154
-# Specificity : 0.9231
-#improvement from 0.80 accuracy to 0.82
 
 #----- rpart and cv to improve the model performance
-train_data_na_omitted <- train_data %>%
-  na.omit()
-
 control <- trainControl(method = "repeatedcv",
                         number = 10,
                         repeats = 2)
 dt_model <-
   train(
     RisicoStudent ~ .,
-    data = train_data_na_omitted,
+    data = train_data,
     method = "rpart",
     trControl = control
   )
@@ -59,30 +46,31 @@ importance <- varImp(dt_model, scale = FALSE)
 plot(importance, cex.lab = 0.5)
 
 formula_importance <- as.formula(
-  "RisicoStudent ~ SPD_Resultaat +
-                      SPD_Gemiddeld + InschrijfStatus + SAQ_Gemiddeld + SAQ_Onvoldoendes +
-                                 Aanwezig"
+  "RisicoStudent ~ WT_Resultaat +
+                      SPD_Resultaat + DB_Onvoldoendes + DB_Gemiddeld + WT_Resultaat +
+                                 WT_Onvoldoendes + DB_NrToetscijfers"
 )
 tree_rpart <-
   rpart(
     formula_importance,
-    data = train_data_na_omitted,
+    data = train_data,
     method = "class",
-    control = rpart.control(minsplit = 20, cp = 0.01)
+    control = rpart.control(minsplit = 20, cp = 0.05)
   )
 
 
 dt_pred <-
-  predict(tree_rpart, train_data_na_omitted, type = "class")
+  predict(tree_rpart, train_data, type = "class")
 confusionMatrix(
   data = dt_pred,
-  reference = train_data_na_omitted$RisicoStudent,
+  reference = train_data$RisicoStudent,
   positive = "1"
 )
-#cp => 0.05 - Accuracy : 0.8634
-#cp => 0.01 - Accuracy : 0.8944
+#cp => 0.05 - Accuracy : 0.8657
+#cp => 0.01 - Accuracy : 0.8771          
 
 ## mooie tree print met rpart
+rpart
 rpart.plot(tree_rpart, type = 4, main = "")
 
 #=====================
@@ -91,11 +79,11 @@ rpart.plot(tree_rpart, type = 4, main = "")
 
 rf_tree <-
   randomForest(RisicoStudent ~ .,
-               data = train_data_na_omitted,
+               data = train_data,
                importance = TRUE)
 rf_tree
 # Call:
-#   randomForest(formula = RisicoStudent ~ ., data = train_data_na_omitted)
+#   randomForest(formula = RisicoStudent ~ ., data = train_data)
 # Type of random forest: classification
 # Number of trees: 500
 # No. of variables tried at each split: 4
@@ -115,37 +103,23 @@ plot(rf_tree)
 pred_rf_tree <- predict(rf_tree, test_data, type = "class")
 confusionMatrix(table(pred_rf_tree, test_data$RisicoStudent), positive =
                   "1")
-# Accuracy : 0.8047          
-
+# Accuracy : 0.8547                      
 
 rf_tree_2 <-
   randomForest(
     RisicoStudent ~ .,
-    data = train_data_na_omitted,
+    data = train_data,
     mtry = 7,
     importance = TRUE
   )
 pred_rf_tree_2 <- predict(rf_tree_2, test_data, type = "class")
 confusionMatrix(table(pred_rf_tree_2, test_data$RisicoStudent), positive =
                   "1")
-
-
-
 #mtry=5
-# Accuracy : 0.7907         
+# Accuracy : 0.8462                   
 
 # mtry=6
-# Accuracy : 0.8047          
+# Accuracy : 0.8504                    
 
 # mtry=7
-# Accuracy : 0.7953          
-
-#pruning
-prune_rf_tree <- prune.rpart(rf_tree, cp = 0.05)
-plot(prune_rf_tree)
-
-pred_tree_pruned_rf <-
-  predict(prune_rf_tree, test_data, type = "class")
-
-confusionMatrix(table(pred_tree_pruned_rf, test_data$RisicoStudent),
-                positive = "1")
+# Accuracy : 0.8547          
