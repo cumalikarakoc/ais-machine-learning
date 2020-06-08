bf113876f1411511eac495fead932833dda9a16e
cumalikarakoc <cumalikarakoc783@hotmail.com>
2020-06-08 14:12:23 +0200

    Factorise columns to avoid the warning regarding 'na's introduced by coercion'. Remove rpart model performance attempts

--- Modeling/globals.R
+++ Modeling/globals.R
@@ -1,14 +1,27 @@
 # split data into training and test datasets in 60:40 ratio
 set.seed(1)
-# remove the columns: vooropleiding, plaats and the courses from 2nd period
+# remove the columns that cause problems due to high number of levels: vooropleiding, vooropleidingdiplomadatum en vooropleidingplaats
 risico_studenten_df_dt <- risico_studenten_df %>%
-  select(-c(5, 7)) %>%
+  select(-c(5, 6, 7)) %>%
   replace(is.na(.), 0)
 
-risico_studenten_df_dt$RisicoStudent <-
-  as.factor(risico_studenten_df_dt$RisicoStudent)
+colums_to_factorise <-
+  c(
+    "RisicoStudent",
+    "Geslacht",
+    "Vooropleidingniveau",
+    "Opleidingsvorm",
+    "InschrijfLocatie",
+    "InschrijfStatus",
+    "VanDatum"
+  )
+for (col in colums_to_factorise) {
+  risico_studenten_df_dt[, col] <- as.factor(risico_studenten_df_dt[, col])
+}
+str(risico_studenten_df_dt)
 
-indexes <- sample(1:nrow(risico_studenten_df), size = 0.6 * nrow(risico_studenten_df_dt))
-train_data <- risico_studenten_df_dt[indexes,]
-test_data <- risico_studenten_df_dt[-indexes,]
-
+indexes <-
+  sample(1:nrow(risico_studenten_df),
+         size = 0.6 * nrow(risico_studenten_df_dt))
+train_data <- risico_studenten_df_dt[indexes, ]
+test_data <- risico_studenten_df_dt[-indexes, ]
--- Modeling/random-forest.R
+++ Modeling/random-forest.R
@@ -11,15 +11,16 @@ plot(tree_fit)
 text(tree_fit , pretty = 0)
 #the most important indicator is SPD_Resultaat because the root is SPD_Resultaat
 
+set.seed(5)
 pred_tree <- predict(tree_fit, test_data, type = "class")
 confusionMatrix(table(pred_tree, test_data$RisicoStudent), positive = "1")
-# Accuracy : 0.7906                     
+# Accuracy : 0.7991                     
 
 cv_tree <- cv.tree(tree_fit, FUN = prune.misclass)
 cv_tree
 plot(cv_tree$size, cv_tree$dev, type = "b")
 
-prune_tree <- prune.misclass(tree_fit, best = 8)
+prune_tree <- prune.misclass(tree_fit, best = 9)
 plot(prune_tree)
 text(prune_tree)
 
@@ -28,55 +29,11 @@ text(prune_tree)
 pred_tree_pruned <- predict(prune_tree, test_data, type = "class")
 confusionMatrix(table(pred_tree_pruned, test_data$RisicoStudent), positive =
                   "1")
-# Accuracy : 0.8205
-
-#----- rpart and cv to improve the model performance
-control <- trainControl(method = "repeatedcv",
-                        number = 10,
-                        repeats = 2)
-dt_model <-
-  train(
-    RisicoStudent ~ .,
-    data = train_data,
-    method = "rpart",
-    trControl = control
-  )
-
-importance <- varImp(dt_model, scale = FALSE)
-plot(importance, cex.lab = 0.5)
-
-formula_importance <- as.formula(
-  "RisicoStudent ~ WT_Resultaat +
-                      SPD_Resultaat + DB_Onvoldoendes + DB_Gemiddeld + WT_Resultaat +
-                                 WT_Onvoldoendes + DB_NrToetscijfers"
-)
-tree_rpart <-
-  rpart(
-    formula_importance,
-    data = train_data,
-    method = "class",
-    control = rpart.control(minsplit = 20, cp = 0.05)
-  )
-
-
-dt_pred <-
-  predict(tree_rpart, train_data, type = "class")
-confusionMatrix(
-  data = dt_pred,
-  reference = train_data$RisicoStudent,
-  positive = "1"
-)
-#cp => 0.05 - Accuracy : 0.8657
-#cp => 0.01 - Accuracy : 0.8771          
-
-## mooie tree print met rpart
-rpart
-rpart.plot(tree_rpart, type = 4, main = "")
+# Accuracy : 0.8291
 
 #=====================
 #==== RANDOM FOREST ==
 #=====================
-
 rf_tree <-
   randomForest(RisicoStudent ~ .,
                data = train_data,
@@ -86,40 +43,30 @@ rf_tree
 #   randomForest(formula = RisicoStudent ~ ., data = train_data)
 # Type of random forest: classification
 # Number of trees: 500
-# No. of variables tried at each split: 4
+# No. of variables tried at each split: 5
 #
 # OOB estimate of  error rate: 15.22%
 # Confusion matrix:
 #   0  1 class.error
 # 0 227 14  0.05809129
 # 1  35 46  0.43209877
-
-importance(rf_tree)
 varImpPlot(rf_tree)
 
-plot(rf_tree)
-
-
 pred_rf_tree <- predict(rf_tree, test_data, type = "class")
-confusionMatrix(table(pred_rf_tree, test_data$RisicoStudent), positive =
-                  "1")
-# Accuracy : 0.8547                      
+confusionMatrix(table(pred_rf_tree, test_data$RisicoStudent), positive = "1")
+# Accuracy : 0.8889                     
 
 rf_tree_2 <-
   randomForest(
     RisicoStudent ~ .,
     data = train_data,
-    mtry = 7,
+    mtry = 4,
     importance = TRUE
   )
 pred_rf_tree_2 <- predict(rf_tree_2, test_data, type = "class")
-confusionMatrix(table(pred_rf_tree_2, test_data$RisicoStudent), positive =
-                  "1")
-#mtry=5
-# Accuracy : 0.8462                   
+confusionMatrix(table(pred_rf_tree_2, test_data$RisicoStudent), positive = "1")
+#mtry=4
+# Accuracy : 0.8846                
 
 # mtry=6
-# Accuracy : 0.8504                    
-
-# mtry=7
-# Accuracy : 0.8547          
+# Accuracy : 0.8846
