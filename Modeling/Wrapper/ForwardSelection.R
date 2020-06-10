b07a490ca08bca5d2c769d675f90dd6d9ae56956
unknown <trabitour01@gmail.com>
2020-06-10 14:35:11 +0200

    forward selection changes

--- Modeling/Wrapper/ForwardSelection.R
+++ Modeling/Wrapper/ForwardSelection.R
@@ -3,7 +3,6 @@
 #===============================
 library(caret)
 risico_studenten_df[is.na(risico_studenten_df)] = 0
-risico_studenten_df = risico_studenten_df[,-c(5,7)]
 risico_studenten_df_Blok1 = risico_studenten_df[,-c(11,14,15,18,19,22,23,26)]
 ctrl <- rfeControl(functions = rfFuncs, method = "cv", number = 10)
 
@@ -69,31 +68,26 @@ step(Blok1_2.FitStart, direction = "forward", scope = formula(Blok1_2.FitAll))
 Blok1_2fs.FitBest <- glm(formula = RisicoStudent ~ WT_Resultaat + InschrijfStatus + 
                            DB_Onvoldoendes + Aanwezig + WT_Onvoldoendes + DB_Resultaat + 
                            SAQ_Onvoldoendes + SPD_Gemiddeld + Afwezig + Opleidingsvorm + 
-                           SPD_Resultaat + Vooropl_diplomadatum + LeeftijdStudent_op_Vandatum + 
-                           DB_NrToetscijfers, data = Blok1_2.train)
+                           SPD_Resultaat, data = Blok1_2.train)
 
-Blok1_2fs.Predict <- predict(Blok1_2fs.FitBest, newdata=Blok1_2.test)
+Blok1_2fs.Predict <- predict(Blok1_2fs.FitBest, newdata=Blok1_2.test, type= "class")
 Blok1_2fs.Predict <- round(Blok1_2fs.Predict)
-confusionMatrix(data=as.factor(Blok1_2fs.Predict), reference=Blok1_2.test_Risico, positive = "1") #0.8504
+confusionMatrix(data=as.factor(Blok1_2fs.Predict), reference=Blok1_2.test_Risico, positive = "1") #0.8547
 
 Blok1_2All.Predict <- predict(Blok1_2.FitAll, newdata=Blok1_2.test)
 Blok1_2All.Predict <- round(Blok1_2All.Predict)
-confusionMatrix(data=as.factor(Blok1_2All.Predict), reference=Blok1_2.test_Risico, positive = "1") #0.859
+confusionMatrix(data=as.factor(Blok1_2All.Predict), reference=Blok1_2.test_Risico, positive = "1") #0.8547
 
 #===============================
 # Recursive Feature Elimination
 #===============================
 
 set.seed(4)
+ctrl <- rfeControl(functions = rfFuncs, method = "cv", number = 10)
 Blok1_2.rfe <- rfe(Blok1_2.train[,-2], as.factor(Blok1_2.train$RisicoStudent), rfeControl = ctrl)
 print(Blok1_2.rfe)
 predictors(Blok1_2.rfe)
 plot(Blok1_2.rfe, type=c("g", "o"))
-
-Blok1_2rfe.FitBest <- glm(formula = RisicoStudent ~ InschrijfStatus + WT_Resultaat + WT_Gemiddeld + DB_Resultaat + 
-                            DB_Gemiddeld + SPD_Resultaat + Aanwezig + WT_Onvoldoendes + SAQ_Gemiddeld + Afwezig + 
-                            DB_Onvoldoendes + SAQ_Onvoldoendes + SPD_Gemiddeld + DB_NrToetscijfers + 
-                            WT_NrToetscijfers + SAQ_Resultaat, data = Blok1_2.train)
 
 Blok1_2rfe.Predict <- predict(Blok1_2rfe.FitBest, newdata=Blok1_2.test)
 Blok1_2rfe.Predict <- round(Blok1_2rfe.Predict)
